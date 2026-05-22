9ec76cb3ffe4f104c693d5a081fa56de09c4deb9
robotHR <eroareproces@gmail.com>
2026-05-22 16:28:42 +0300

    Update routes.py

--- app/api/routes.py
+++ app/api/routes.py
@@ -908,7 +908,7 @@ async def gmail_and_analyze(target_job: str = Form(...)):
     moved_files = temporarily_keep_only_files(downloaded_files)
 
     try:
-    pass  # TODO: Migrate to V2 system
+        pass  # TODO: Migrate to V2 system
     # analysis_result = process_cvs_for_job(target_job)
 finally:
     restore_temp_files(moved_files)
